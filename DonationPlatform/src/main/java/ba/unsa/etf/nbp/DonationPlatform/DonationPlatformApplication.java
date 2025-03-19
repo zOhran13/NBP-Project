edbca8f5089181d969361170de7c862f1b2fb4d0
Zlata Ohran <zohran1@etf.unsa.ba>
2025-03-19 12:58:09 +0100

    first commit

---
+++ DonationPlatform/src/main/java/ba/unsa/etf/nbp/DonationPlatform/DonationPlatformApplication.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nbp.DonationPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationPlatformApplication.class, args);
+	}
+
+}
---
+++ DonationPlatform/src/test/java/ba/unsa/etf/nbp/DonationPlatform/DonationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nbp.DonationPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
