0b337fbc7e49d05b2d97fe0553db601803ce37df
Zlata Ohran <zohran1@etf.unsa.ba>
2025-05-13 23:39:24 +0200

    fixed reset password

--- DonationPlatform/src/main/java/ba/unsa/etf/nbp/DonationPlatform/service/EmailService.java
+++ DonationPlatform/src/main/java/ba/unsa/etf/nbp/DonationPlatform/service/EmailService.java
@@ -1,21 +1,47 @@
 package ba.unsa.etf.nbp.DonationPlatform.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailService {
-    @Autowired
-    private JavaMailSender mailSender;
 
-    public void sendResetPasswordEmail(String to, String resetLink) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Password Reset Request");
-        message.setText("Click the link to reset your password: " + resetLink);
-        mailSender.send(message);
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
+
+    @Value("${sendgrid.sender.email}")
+    private String senderEmail;
+
+    public void sendResetPasswordEmail(String to, String resetUrl) {
+        Email from = new Email(senderEmail);
+        Email toEmail = new Email(to);
+        String subject = "Resetuj lozinku";
+        String contentString = "<p>Kliknite na sljedeći link da resetujete lozinku:</p>"
+                + "<a href=\"" + resetUrl + "\">Resetuj lozinku</a>";
+        Content content = new Content("text/html", contentString);
+
+        Mail mail = new Mail(from, subject, toEmail, content);
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            System.out.println("Status: " + response.getStatusCode());
+            System.out.println("Body: " + response.getBody());
+            System.out.println("Headers: " + response.getHeaders());
+
+        } catch (IOException e) {
+            System.err.println("Greška prilikom slanja e-maila: " + e.getMessage());
+        }
     }
 }
-
